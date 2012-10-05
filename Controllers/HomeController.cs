95b6548f9966abd7b99ba41ecde4b29c403adb84
andrey.lugkovsky <andrey.lugkovsky@paralect.com>
2012-10-05 19:01:49 +0300

    init commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace jsApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        public ActionResult GetTemplatets()
+        {
+            return Json(new
+                            {
+                                templates = TemplatesService.GetAll()
+                            }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GetTemplate(string id)
+        {
+            return Json(TemplatesService.GetBy(id), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GetDashbord()
+        {
+            var items = new List<MenuItem>
+                            {
+                                new MenuItem{ Name = "Letters templates", Url = "#templates"},
+                                new MenuItem{ Name = "Media Box Setup", Url = "#mediaboxsetup"}
+                            };
+            return Json(new { items }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GetMenu()
+        {
+            var items = new List<MenuItem>
+                            {
+                                new MenuItem {Name = "Letters templates", Url = "#templates"},
+                                new MenuItem {Name = "Media Box Setup", Url = "#mediaboxsetup"}
+                            };
+            return Json(items, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GetMediaBoxData()
+        {
+            return Json("hello", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
+
+public class TemplatesService
+{
+    static readonly List<Template> _templates = new List<Template>
+                                           {
+                                               new Template{ Id = "1", Name = "Name1", Description = "Description1"},
+                                               new Template{ Id = "2", Name = "Name2", Description = "Description2"},
+                                           }; 
+    public static List<Template> GetAll()
+    {
+        return _templates;
+    }
+
+    public static Template GetBy(string id)
+    {
+        return _templates.FirstOrDefault(_ => _.Id == id);
+    }
+}
+
+public class Template
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
+
+public class MenuItem
+{
+    public string Name { get; set; }
+    public string Url { get; set; }
+}
---
+++ Controllers/TemplatesController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace jsApp.Controllers
+{
+    public class TemplatesController : Controller
+    {
+        public ActionResult GetTemplate(string templateId)
+        {
+            return PartialView((object) templateId);
+        }
+
+    }
+}
