9bd5bf90b257f5e00f1605bf38ff99217a4aeba8
andrey.lugkovsky <andrey.lugkovsky@paralect.com>
2012-10-10 19:14:12 +0300

    rename

--- js/buildPage.js
+++ js/buildPage.js
@@ -1,11 +1,11 @@
-﻿define(["jquery", "knockout", "/js/ViewModel.js", "text"], function ($, ko, ViewModel) {
-    return function (screen, responseData, callback) {
-        var layoutName = screen.layout;
+﻿define(["jquery", "knockout", "/js/Screen.js", "text"], function ($, ko, Screen) {
+    return function (screenOptions, responseData, callback) {
+        var layoutName = screenOptions.layout;
         if (layoutName) {
             require(["/js/layouts/" + layoutName + ".js", 'text!/js/templates/_' + layoutName + ".html"], function (layoutViewModel, layoutHtml) {// if screen have layout load it resurces
                 var layoutData = new layoutViewModel(responseData);
-                var vml = new ViewModel(screen, responseData, layoutData, function (screenSection) {
-                    layoutData.content = screenSection; // layout vm should contain content observable, for render screen inside
+                var vml = new Screen(screenOptions, responseData, layoutData, function (screen) {
+                    layoutData.content = screen; // layout vm should contain content observable, for render screen inside
 
                     callback({
                         html: ko.observable(layoutHtml),
@@ -16,8 +16,8 @@
 
 
         } else {
-            var vm = new ViewModel(screen, responseData, null, function (screenSection) {
-                callback(screenSection);
+            var vm = new Screen(screenOptions, responseData, null, function (screen) {
+                callback(screen);
             });
 
         }
